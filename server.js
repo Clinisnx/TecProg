6f8b0a58ad72e22c1fbaceca3fbef5a1c99856e0
clinisnx <nairam2004@gmail.com>
2023-05-31 12:27:56 -0300

    Arquivos base criados

--- server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./app');
-
-require('dotenv').config({path:'variables.env'});
-
-app.set('port', process.env.PORT || 7777); //escolha uma porta padrão
-const server = app.listen(app.get('port'),()=>{
-    console.log("O servidor está rodando na porta: "+server.address().port);
-});
